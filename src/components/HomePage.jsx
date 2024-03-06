9fcc9e8d55454b9cee4a443f3c633dd8bcb17741
Ragul_8775 <101054801+Ragul8775@users.noreply.github.com>
2024-03-06 20:00:42 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "../widgets/NavBar";
 import Products from "../widgets/Products";
+import { IoConstructSharp } from "react-icons/io5";
 
 const HomePage = () => {
   return (
     <div className=" bg-gradient-to-r from-primary to-secondary min-h-screen">
-      <NavBar />
-      <Products />
+      {/* <NavBar />
+      <Products /> */}
+      <div className= " flex items-center justify-center">
+      <IoConstructSharp />
+      Under Construction
+      </div>
     </div>
   );
 };
